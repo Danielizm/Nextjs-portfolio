44f2946ff3dbebc3ec5b4af7c58f7d9a9fd3bda0
Daniel Su <20549850+Danielizm@users.noreply.github.com>
2023-02-28 22:31:21 +1100

    add 404

---
+++ src/pages/404.tsx
@@ -0,0 +1,16 @@
+//import React from "react"
+import Layout from "../components/layout"
+//import SEO from "../components/seo"
+
+const NotFoundPage = () => (
+  <Layout>
+    <div className="not-found">
+    <div className="wrap">
+    <h1>404</h1>
+    <p>Ooops! The page doesn't exist.</p>
+    </div>
+    </div>
+  </Layout>
+)
+
+export default NotFoundPage
