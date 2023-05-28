3061f8a7f950d04d6e398451d5f25c812101f2b7
Daniel Su <20549850+Danielizm@users.noreply.github.com>
2023-05-28 23:21:02 +1000

    get projects test

--- src/pages/work.tsx
+++ src/pages/work.tsx
@@ -1,10 +1,24 @@
+import { useEffect, useState } from "react";
 import { getProjects } from "../../sanity/sanity-utils";
 
 export default function Work() {
-    //const projects = await getProjects();
+    const [projects, setProjects] = useState([])
+    useEffect( () => { 
+        async function fetchData(){
+            try{
+            const posts = await getProjects();
+            setProjects(posts) 
+            }catch(err){
+                console.log(err)
+            }
+        } 
+        fetchData(); 
+    }, []);
     return (
         <div>
-            
+            {projects.map((project)=>(
+                <div key={project._id}>{project.name}</div>
+            ))}
         </div>
     )
 }
